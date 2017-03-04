7be8b1c7e5f3ef37b83da68471ab895a06aa1078
GemShi <gemshi@GemShideMacBook-Pro.local>
2017-03-04 18:09:59 +0800

    Initial Commit

---
+++ GemNSOperationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GemNSOperationDemo
+//
+//  Created by GemShi on 2017/3/4.
+//  Copyright © 2017年 GemShi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GemNSOperationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GemNSOperationDemo
+//
+//  Created by GemShi on 2017/3/4.
+//  Copyright © 2017年 GemShi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
